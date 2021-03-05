aaeb0b82b01a9ca5e68bbacd421d5a7047ae1c2f
yokesh20 <80085878+yokesh20@users.noreply.github.com>
2021-03-05 11:04:27 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup
+    name='gingee',
+    version='1.1.0.0',
+    packages=['main'],
+    url='https://github.com/yokesh20/gingee-trip',
+    license='approved by deepsthal',
+    author='yokesh',
+    author_email='yokeshnatrajan@gmail.com',
+    description='gingee'
+)
